d4508ecaebc9b763dda3b27815771291dbb5bd45
lippesouzza <84047056+lippesouzza@users.noreply.github.com>
2022-08-30 18:03:57 -0300

    Inicializando o React

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Home.css';
+
+function Home() {
+    return (
+        <>
+            <h1 className="titulo">Lorem ipsum dolor sit amet, consectetur</h1>
+            <img src="https://i.imgur.com/H88yIo2.png" alt="Paisagem" className="img" />
+        </>
+    )
+}
+
+export default Home;
